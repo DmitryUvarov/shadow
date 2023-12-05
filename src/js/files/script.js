879acf935c2026f17107c100c888a98aa7cdb791
Dima <uvarov3123@gmail.com>
2023-12-05 16:26:47 +0200

    add header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,44 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoad)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoad() {
+    const htmlTag = document.documentElement
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    document.addEventListener('click', e => {
+        const targetElement = e.target
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+        if (document.querySelector('.menu__item.open') && !targetElement.closest('.menu__item.open')) {
+            document.querySelector('.menu__item.open').classList.remove('open')
+        }
+
+
+        if (targetElement.closest('.menu__link') && htmlTag.closest('.touch')) {
+            let menuItem = targetElement.closest('.menu__item')
+
+            if (targetElement.closest('.menu__item.open')) {
+                menuItem.classList.remove('open')
+                return
+            }
+            if (document.querySelector('.menu__item.open') && !targetElement.closest('.menu__item.open')) {
+                document.querySelector('.menu__item.open').classList.remove('open')
+                menuItem.classList.add('open')
+                return
+            }
+            if (!targetElement.closest('.menu__item.open')) {
+                menuItem.classList.add('open')
+                if (!targetElement.closest('._spoller-init')) {
+                    e.preventDefault();
+                    console.log('1');
+                }
+            }
+            // if (targetElement.closest('.menu__item')) {
+            //     menuItem.classList.remove('open')
+            // }
+
+
+
+        }
+    })
+}
